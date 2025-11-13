cc3f84c9a413900f6ebb12170c0f53865f91b0d3
Akshayaa <akshayaa1302@gmail.com>
2025-11-13 12:45:18 +0800

    Fix

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,18 +1,9 @@
 /* eslint-env node */
 
-/* eslint-env node */
-
 import { test, expect } from '@playwright/test';
-import path from 'path';
 
 test('button click updates message', async ({ page }) => {
-  // Build an absolute path to index.html
-  const filePath = path.resolve(__dirname, '../index.html');
-
-  // Convert Windows-style paths (C:\...) to a valid file:// URL
-  const fileUrl = 'file:///' + filePath.replace(/\\/g, '/');
-
-  await page.goto(fileUrl);
+  await page.goto('file://../index.html');
 
   await page.click('#clickMe');
   await expect(page.locator('#message')).toHaveText('Button clicked!');
