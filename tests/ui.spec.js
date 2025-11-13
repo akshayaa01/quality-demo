639867ccb1add1d632bb70147b4fc7a661a55076
Akshayaa <akshayaa1302@gmail.com>
2025-11-13 12:30:43 +0800

    Add GitHub Actions CI pipeline

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,13 +1,11 @@
-import { test, expect } from '@playwright/test'; 
+/* eslint-env node */
 
- 
+import { test, expect } from '@playwright/test';
 
-test('button click updates message', async ({ page }) => { 
+test('button click updates message', async ({ page }) => {
+  await page.goto('file://../index.html');
 
-  await page.goto('file://' + __dirname + '/../index.html'); 
-
-  await page.click('#clickMe'); 
-
-  await expect(page.locator('#message')).toHaveText('Button clicked!'); 
-
+  await page.click('#clickMe');
+  await expect(page.locator('#message')).toHaveText('Button clicked!');
 });
+
